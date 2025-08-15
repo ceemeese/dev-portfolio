3fbc06060bc67858819eceae05655e4feafb9e66
ceemeese <cmsalvador92@gmail.com>
2025-08-15 22:03:57 +0200

    Add: filtro de skills en función de selección

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-
 import vue from '@astrojs/vue';
-
 
 import icon from 'astro-icon';
 
--- src/consts/stacknav.ts
+++ src/consts/stacknav.ts
@@ -1,14 +1,12 @@
 import type { navItem } from '../types/stacknavType';
 
-const navItems: navItem[] = [
+export const NAVITEMS: navItem[] = [
 
     { title: 'Todo', label: 'Todo', icon: 'star', id:'todo' },
-    { title: 'Soft Skills', label: 'Soft Skills', icon: 'sprout', id:'soft-skills' },
+    { title: 'Soft Skills', label: 'Soft Skills', icon: 'sprout', id:'soft' },
     { title: 'Frontend', label: 'Frontend', icon: 'code-xml', id:'frontend' },
     { title: 'Backend', label: 'Backend', icon: 'code-xml', id:'backend' },
     { title: 'Base de datos', label: 'Base de datos', icon: 'database', id:'database' },
-    { title: 'Devops & Cloud', label: 'Devops & Cloud', icon: 'cloud', id:'devops-cloud' },
-    { title: 'Tools', label: 'Tools', icon: 'settings', id:'Tools' }
+    { title: 'Devops & Cloud', label: 'Devops & Cloud', icon: 'cloud', id:'cloud' },
+    { title: 'Tools', label: 'Tools', icon: 'settings', id:'tools' }
 ];
-
-export default navItems;
